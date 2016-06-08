281436b0a013fc31762689e45f558e9cd4c88464
Sanai <sanaimaity@gmail.com>
2016-06-08 14:30:23 +0530

    Commpleted area finder (Polymorphism)

---
+++ Problems/Area.cs
@@ -0,0 +1,40 @@
+using System;
+
+
+namespace Problems
+{
+  interface Shape{
+		 double area(int a);
+	}
+
+	class Circle : Shape{
+		public double area(int r){
+			double result = 0.0d;
+			result = System.Math.PI * r * r;
+			return result;
+		}
+	}
+		class Square : Shape{
+		public double area(int a){
+				double result = 0.0d;
+				result = a * a;
+				return result;
+		}
+
+	}
+
+	public class Area
+	{		
+		public  static void Main(){
+		
+				double answer = 0.0d;
+				Circle obj = new Circle ();
+
+				answer = obj.area (34);
+				Console.WriteLine ("{0}", answer);
+
+			}
+
+	}
+}
+
