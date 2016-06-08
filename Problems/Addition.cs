03389c6543549a06d61c596ce66917cc55c3f488
Sanai <sanaimaity@gmail.com>
2016-06-08 16:17:57 +0530

    Updated the Addition file

--- Problems/Addition.cs
+++ Problems/Addition.cs
@@ -2,10 +2,20 @@ using System;
 
 namespace Problems
 {
+	class Arithmatic{
+	public double add(int a,int b){
+			int c = 0 ;
+			c = a+b;
+			return c;
+		}
+	
+	}
 	public class Addition
 	{
-		public Addition ()
-		{
+		public static void Main(){
+			Arithmatic obj = new Arithmatic ();
+
+			Console.WriteLine ("{0}", obj.add (24, 45));
 		}
 	}
 }
